43500af3a4fce63ce1b0fc46d86e8c6cecd3350c
Jeonhui <48307153+Jeonhui@users.noreply.github.com>
2024-07-16 01:02:22 +0900

    환경 변수 설정 순서 변경

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
+
 import os
 from dotenv import load_dotenv
-from routers.v1.DiaryRouter import DiaryRouter
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
+from routers.v1.DiaryRouter import DiaryRouter
+
 app = FastAPI()
 
 app.include_router(DiaryRouter)
+
 
 @app.get("/")
 async def root():
